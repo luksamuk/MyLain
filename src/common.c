d718fcc37a4710b27169645388f5302d76cd5085
Lucas Vieira <lucas.samuel2002@gmail.com>
2017-07-22 09:16:52 -0300

    Fixed a dumb thing on the files' license notice.

--- src/common.c
+++ src/common.c
@@ -10,7 +10,7 @@
  *  option) any later version.                                    *
  *                                                                *
  *  You should have received a copy of the GNU Lesser General     *
- *  Public License along with OficinaFramework.  If not, see      *
+ *  Public License along with MyLain. If not, see                 *
  *  <http://www.gnu.org/licenses/>.                               *
  ******************************************************************/
 
