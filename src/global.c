a3e9efe8b09e5fa45a11340be9b1f7d327866338
Lucas Vieira <lucas.samuel2002@gmail.com>
2017-07-28 10:44:56 -0300

    Another fix on documentation. What a shame.

--- src/global.c
+++ src/global.c
@@ -2,11 +2,11 @@
  *  Copyright (c) 2017 Lucas Vieira <lucas.samuel2002@gmail.com>  *
  *  This file is part of MyLain.                                  *
  *                                                                *
- *  OficinaFramework is free software: you can redistribute       *
- *  it and/or modify it under the terms of the GNU Lesser         *
- *  General Public License as published by the Free Software      *
- *  Foundation, either version 3 of the License, or (at your      *
- *  option) any later version.                                    *
+ *  MyLain is free software: you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public    *
+ *  License as published by the Free Software Foundation, either  *
+ *  version 3 of the License, or (at your option) any later       *
+ *  version.                                                      *
  *                                                                *
  *  You should have received a copy of the GNU Lesser General     *
  *  Public License along with MyLain. If not, see                 *
--- src/repl.c
+++ src/repl.c
@@ -1,3 +1,19 @@
+/******************************************************************
+ *  Copyright (c) 2017 Lucas Vieira <lucas.samuel2002@gmail.com>  *
+ *  This file is part of MyLain.                                  *
+ *                                                                *
+ *  MyLain is free software: you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public    *
+ *  License as published by the Free Software Foundation, either  *
+ *  version 3 of the License, or (at your option) any later       *
+ *  version.                                                      *
+ *                                                                *
+ *  You should have received a copy of the GNU Lesser General     *
+ *  Public License along with MyLain. If not, see                 *
+ *  <http://www.gnu.org/licenses/>.                               *
+ ******************************************************************/
+
+
 #include "mylain/repl.h"
 #include "mylain/global.h"
 #include "mylain/lexer.h"
